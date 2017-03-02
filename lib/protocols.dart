889d516c7bb3e9025c6ded5561a4b0d49165b023
axetroy <troy450409405@gmail.com>
2017-03-02 12:15:55 +0800

    docs: update protocols function desc

--- lib/protocols.dart
+++ lib/protocols.dart
@@ -1,8 +1,7 @@
 library protocols;
 
 /**
- * 获取url地址的协议proto
- * 返回一个字符串/数组
+ * parse a input url and return a protocols List
  */
 List<String> protocols(String url) {
   String match = new RegExp(r'^\s?[\w\+]+(?=\:\/\/)|[\w\+]+(?=\@)').stringMatch(url);
